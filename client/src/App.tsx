66619c7304b53bbf9223338d8e2c6e15f5ab0a32
Jake Hubbard <49132883+jdhubb99@users.noreply.github.com>
2023-03-21 03:23:22 +0000

    feat: Add basic navbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar/Navbar';
+import { SITE_TITLE } from './consts';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Navbar title={SITE_TITLE} />
+      <main className="pages">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
