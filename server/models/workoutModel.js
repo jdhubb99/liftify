48a2cdf583ed6f386dbbc68780c90258c169b7fa
Jake Hubbard <49132883+jdhubb99@users.noreply.github.com>
2023-03-21 00:27:16 +0000

    Add workoutModel and Workout schema to server

---
+++ server/models/workoutModel.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    title: { type: String, required: true },
+    reps: { type: Number, required: true },
+    weight: { type: Number, required: true },
+}, { timestamps: true });
+
+export const Workout = mongoose.model('Workout', workoutSchema);
--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -1,10 +1,16 @@
 import express from 'express';
-
+import { Workout } from '../models/workoutModel.js';
 const router = express.Router();
 
 // GET all workouts
-router.get('/', (req, res) => {
-  res.json({mssg: 'GET all workouts'});
+router.get('/', async (req, res) => {
+  try {
+    const workouts = await Workout.find();
+    res.status(200).json(workouts);
+  }
+  catch (error) {
+    res.status(400).json({error: error.message});
+  }
 });
 
 // GET a single workout
@@ -13,8 +19,14 @@ router.get('/:id', (req, res) => {
 });
 
 // POST a new workout
-router.post('/', (req, res) => {
-  res.json({mssg: 'POST a new workout'});
+router.post('/', async (req, res) => {
+  const { title, reps, weight } = req.body;
+  try {
+    const workout = await Workout.create({ title, reps, weight});
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({error: error.message});
+  }
 });
 
 // DELETE a workout
