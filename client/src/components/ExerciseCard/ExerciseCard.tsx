b504ba8a5ccfda857092dde83afd447104bdfb65
jdhubb99 <jacobdhubbard@gmail.com>
2023-03-28 00:32:47 -0400

    feat: Add basic delete button to exercise cards

--- client/src/components/ExerciseCard/ExerciseCard.tsx
+++ client/src/components/ExerciseCard/ExerciseCard.tsx
@@ -1,11 +1,14 @@
 import { Exercise } from '../../types/Exercise';
 import './ExerciseCard.css';
+import { useExerciseContext } from '../../hooks/useExerciseContext';
 
 interface Props {
   exercise: Exercise;
 }
 
 const ExerciseCard: React.FC<Props> = ({ exercise }) => {
+  const { dispatch } = useExerciseContext();
+
   function formatDate(date: string | number | Date) {
     const newDate = new Date(date);
     const formattedDate = newDate.toLocaleDateString('en-US', {
@@ -20,6 +23,18 @@ const ExerciseCard: React.FC<Props> = ({ exercise }) => {
     });
     return `${formattedTime} on ${formattedDate}`;
   }
+  
+  const handleClick = async () => {
+    const response = await fetch('http://localhost:4000/api/exercises/' + exercise._id, {
+      method: 'DELETE'
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_EXERCISE', payload: data });
+    }
+  }
 
   return (
     <div className="exercise-card">
@@ -32,11 +47,12 @@ const ExerciseCard: React.FC<Props> = ({ exercise }) => {
         <strong>Reps: </strong>
         {exercise.reps}
       </p>
-      <p>
-        {exercise.createdAt
-          ? formatDate(exercise.createdAt)
-          : exercise.createdAt}
-      </p>
+        <p>
+          {exercise.createdAt
+            ? formatDate(exercise.createdAt)
+            : exercise.createdAt}
+        </p>
+        <button onClick={handleClick}>Delete</button>
     </div>
   );
 };
